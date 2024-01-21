3085452aea1d027c8bef1e4fee89db7f8d2f7282
Abyantahta <abyantahta2002@gmail.com>
2024-01-21 17:00:33 +0700

    read todos setup finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Auth from './components/Auth.js';
+import {db} from './config/firebase.js'
+import { getDocs, collection } from 'firebase/firestore';
 function App() {
+  const [todos,setTodos] = useState([])
+
+  const todosCollectionRef = collection(db,'todos')
+  const getTodos = async () =>{
+    console.log('lewat sini')
+  //READ THE DATA
+  //SET THE TODOS
+  try {
+    const datas = await getDocs(todosCollectionRef)
+    console.log(datas)
+    const filteredData = datas.docs.map((doc)=>({
+      ...doc.data(),
+      id : doc.id
+    }))
+    setTodos(filteredData)
+    
+  } catch (error) {
+    console.log(error)
+  }
+  }
+  
+  useEffect(()=>{
+    getTodos();
+  },[])
   return (
     <div className="App">
       <Auth/>
-      <h1>hello world</h1>
+      <div className="">
+        {todos.map((todo)=>(
+          <div className="">
+            <h1>{todo.title}</h1>
+            <p>{todo.description}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,16 +1,31 @@
 import { useState } from "react"
-import { auth } from "../config/firebase"
-import { createUserWithEmailAndPassword } from "firebase/auth"
+import { auth, googleProvider } from "../config/firebase"
+import { createUserWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth"
 
 function Auth() {
     const [email,setEmail] = useState('')
     const [password,setPassword] = useState('')
+    
+    console.log(auth?.currentUser?.email)
     const signIn = async () =>{
-        // console.log(auth?.currentUser.email)
         try {
             await createUserWithEmailAndPassword(auth,email,password)
         } catch (err) {
             console.error(err)
+        }
+    }
+    const signInWithGoogle = async () =>{
+        try {
+            await signInWithPopup(auth,googleProvider)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+    const logout = async () =>{
+        try {
+            await signOut(auth)
+        } catch (err) {
+            console.log(err)
         }
     }
 
@@ -19,6 +34,8 @@ function Auth() {
         <input type="email" placeholder="Email..." onChange={(e)=>setEmail(e.target.value)}/>
         <input type="password" placeholder="Password..." onChange={(e)=>setPassword(e.target.value)}/>
         <button onClick={signIn}>Sign in</button>
+        <button onClick={signInWithGoogle}>Sign in with Google</button>
+        <button onClick={logout}>Logout</button>
     </div>
     )
 }
